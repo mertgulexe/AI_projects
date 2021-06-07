d8f9f93c81c528125fa7d27a25db5b0de37d46cd
Mert Gül <gul.mert89@gmail.com>
2021-06-07 20:33:15 +0300

    Update the tutorial code

--- flask101/api/api.py
+++ flask101/api/api.py
@@ -6,29 +6,65 @@ app.config["DEBUG"] = True
 
 books = [
     {'id': 0,
-    'title': 'A Fire Upon the Deep',
-    'author': 'Vernor Vinge',
-    'first_sentence': 'The coldsleep itself was dreamless.',
-    'year_published': '1992'}, 
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
     {'id': 1,
-    'tile': 'The Ones Who Walk Away From Omelas',
-    'author': 'Ursula K. Le Guin',
-    'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-    'published': '1973'},
+     'tile': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
     {'id': 2,
-    'title': 'Dhalgren',
-    'author': 'Samuel R. Delany',
-    'first_sentence': 'to wound the autumnal city.',
-    'published': '1975'}
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
 ]
+
 
 @app.route("/", methods=["GET"])
 def home():
     return """<h1>Distant Reading Archive</h1>
 <p>A prototype API for distant reading of science fiction novels.</p>"""
 
+
 @app.route("/api/v1/resources/books/all", methods=["GET"])
 def api_all():
     return jsonify(books)
 
+
+@app.route("/api/v1/resources/books", methods=["GET"])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if "id" in request.args:
+        id = int(request.args["id"])
+        # example: ?id=0
+        # Data passed through URLs like this (after the ?)
+        # are called query parameters.
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for book in books:
+        if book["id"] == id:
+            results.append(book)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
+
 app.run()
+
+
+# Mert's Notes:
+# 1) After incorporating these design improvements,
+# a request to our API might look like this:
+# https://api.example.com/v1/resources/books?id=10
